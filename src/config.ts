d630646f0f47058e892a225dd94238203fa1841f
rmdocherty <rmdocherty2@gmai.com>
2024-04-20 15:12:03 +0100

    experiments in scaling

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 import { GameScene } from './scenes/game-scene';
 
+// TODO: have 2 different configs for mobile and desktop?
 export const GameConfig: Phaser.Types.Core.GameConfig = {
   title: 'Orcball',
   url: 'https://github.com/rmdocherty/orcball_ts',
@@ -7,11 +8,12 @@ export const GameConfig: Phaser.Types.Core.GameConfig = {
   backgroundColor: 0x3a404d,
   type: Phaser.AUTO,
   scale: {
-    mode: Phaser.Scale.MAX_ZOOM,
-    autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+    mode: Phaser.Scale.FIT, // maybe use RESIZE mode?
+    autoCenter: Phaser.Scale.CENTER_BOTH, // CENTER_HORIZONTALLY
     parent: 'game',
-    width: '100%',
-    height: '100%'
+    // was both '100% - below are iphone SE dims times 2
+    width: 375 * 2,
+    height: 667 * 2
   },
   physics: {
     default: 'arcade',
