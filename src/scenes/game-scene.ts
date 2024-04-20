aa3536288a55108128d817fc07250c7a7d696b7a
rmdocherty <rmdocherty2@gmai.com>
2024-04-20 17:49:33 +0100

    added dot drawing

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -1,11 +1,12 @@
 import { Redhat } from '../objects/redhat';
 import { GraphicDot } from '../objects/dots';
-import { init } from '../logic/board';
+import { LogicGame } from '../logic/board';
 import { Dot } from '../interfaces/shared';
 
 export class GameScene extends Phaser.Scene {
   private myRedhat: Redhat;
-  private myDot: GraphicDot;
+  private gfxDots: GraphicDot[];
+  private logicGame: LogicGame;
 
   constructor() {
     super({ key: 'GameScene' });
@@ -17,22 +18,21 @@ export class GameScene extends Phaser.Scene {
   }
 
   create(): void {
-    const emitter = this.add.particles(0, 0, 'redParticle', {
-      speed: 100,
-      scale: { start: 0.5, end: 0 },
-      blendMode: 'ADD'
-    });
+    this.logicGame = new LogicGame(11, 9);
+    this.gfxDots = this.initDots(this.logicGame)
+  }
 
-    this.myRedhat = new Redhat({
-      scene: this,
-      x: 400,
-      y: 300,
-      texture: 'redhat'
-    });
-
-    this.myDot = new GraphicDot({ scene: this, logicPos: { x: 1, y: 1 }, val: Dot.EMPTY })
-
-    emitter.startFollow(this.myRedhat);
-    init();
+  initDots(game: LogicGame): GraphicDot[] {
+    const dots: GraphicDot[] = []
+    for (let y = 0; y < game.grid.h; y++) {
+      for (let x = 0; x < game.grid.w; x++) {
+        const val = game.grid.get(x, y);
+        if (val != Dot.VOID) {
+          const tmpDot = new GraphicDot({ scene: this, logicPos: { x: x, y: y }, val: val });
+          dots.push(tmpDot);
+        }
+      }
+    }
+    return dots;
   }
 }
